017814a809e85d93d7de14f6ff240b5fd42289de
Banerjee, Ritwik (US - Bengaluru) <ritwbanerjee@deloitte.com>
2017-10-28 23:32:34 +0530

    Remove source map

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -6,7 +6,6 @@ var commonConfig = require('./webpack.common.js');
 var helpers = require('./helpers');
 
 module.exports = webpackMerge(commonConfig, {
-  devtool: 'source-map',
 
   output: {
     path: helpers.root('dist'),
